25c9c49ba012caff9e35bf66a8952aed503547b3
mathe-man <mathieumousset6@gmail.com>
2026-01-21 22:44:47 +0100

    Add the bases of the interface system

---
+++ interface.py
@@ -0,0 +1,42 @@
+# MIT License
+# Copyright (c) 2026 mathe man
+#
+# This source file is licensed under the MIT License.
+# You may use, modify, and distribute it freely.
+
+
+def space(size: int = 1) -> None:
+    if size == 1:
+        print()
+        return
+
+    for i in range(size):
+        print()
+
+
+def getkey() -> str:
+    return input()[0]
+
+
+def choose(options) -> int:
+    for i in range(len(options)):
+        print(str(i+1) + "] " + options[i])
+
+    print("> ", end="")
+
+    answer = getkey()
+    jump = not answer.isdigit()
+
+    while not jump:
+        print("Answer is invalide: " + answer)
+        print("Retry")
+        print("> ", end="")
+        answer = getkey()
+
+        # Get out of the while loop when the answer is a digit and in the range of the choices
+        if answer.isdigit():
+            if int(answer) -1 < len(options):
+                jump = True
+
+
+    return int(answer)
---
+++ main.py
@@ -0,0 +1,26 @@
+# MIT License
+# Copyright (c) 2026 mathe man
+#
+# This source file is licensed under the MIT License.
+# You may use, modify, and distribute it freely.
+
+
+set_debug = True
+from interface import *
+
+def main():
+    print("Nothing there for the moment...")
+
+def debug():
+    print(" == DEBUG == ")
+    print(choose(["mon caca", "ton pipi"]))
+
+
+
+
+
+if set_debug:
+    debug()
+else:
+    main()
+
