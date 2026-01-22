15d03deec473ed0fbf24c91438f47cab4a8dafd6
mathe-man <mathieumousset6@gmail.com>
2026-01-22 12:07:13 +0100

    Add menu for the pythagoras file

--- pythagoras.py
+++ pythagoras.py
@@ -1,5 +1,4 @@
 import math
-
 import interface
 
 
@@ -16,4 +15,26 @@ def side(hypotenuse: float, other: float) -> float:
     return math.sqrt(squared_side(hypotenuse, other))
 
 
+
+def menu():
+    i = interface.choose(["Hypotenuse", "Side", "Squared hypotenuse", "Squared side"])
+
+    if i == 0:
+        floats = interface.get_floats(["a side", "b side"])
+        hypotenuse(floats[0], floats[1])
+
+    elif i == 1:
+        floats = interface.get_floats(["hypotenuse", "other side"])
+        side(floats[0], floats[1])
+
+    elif i == 2:
+        floats = interface.get_floats(["a side", "b side"])
+        squared_hypotenuse(floats[0], floats[1])
+
+    elif i == 1:
+        floats = interface.get_floats(["hypotenuse", "other side"])
+        side(floats[0], floats[1])
+
+    else:
+        pass
 
