a47a549f280dd08419cc01e59722e099d766c1ee
mathe-man <mathieumousset6@gmail.com>
2026-01-22 20:55:39 +0100

    Add base swiper

---
+++ base_swipe.py
@@ -0,0 +1,17 @@
+
+#TODO add security checks (no inputs greater than his base, ...)
+def base_swipe(in_base_size: int, out_base_size, inputs):
+
+    total = 0
+    for i in range(len(inputs)):
+        total += inputs[i] * in_base_size**i
+
+    if out_base_size == 10:
+        return total
+
+    outputs = []
+    while total != 0:
+        outputs.insert(0, (total % out_base_size))
+        total = total // out_base_size
+
+    return outputs
